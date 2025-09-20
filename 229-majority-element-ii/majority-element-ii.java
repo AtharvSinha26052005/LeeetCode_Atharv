f8b5018a8d9e4f2ffb932f32e9b1a032b2145fb5
Atharv Sinha <73791750+AtharvSinha26052005@users.noreply.github.com>
2025-09-20 19:01:16 +0530

    Time: 3 ms (75.07%) | Memory: 50.7 MB (9.37%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,20 +1,40 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) 
     {
+       int c1=0,c2=0;
+       int el1=Integer.MIN_VALUE,el2=Integer.MIN_VALUE;
+
+       for(int i=0;i<nums.length;i++)
+       {
+        if(c1==0 && el2!=nums[i])
+        {
+            el1=nums[i];c1=1;
+        }
+
+        else if(c2==0 && el1!=nums[i])
+        {
+            el2=nums[i];c2=1;
+        }
+
+       else if(el1==nums[i]) c1++;
+       else if (el2==nums[i]) c2++;
+       else {c1--;c2--;}
+       }
+
         List<Integer> ans= new ArrayList<>();
-        Map<Integer,Integer> mpp = new HashMap<>();
+        int cn1=0,cn2=0;
 
         for(int i=0;i<nums.length;i++)
         {
-            mpp.put(nums[i],mpp.getOrDefault(nums[i],0)+1);
+            if(nums[i]==el1) cn1++;
+            else if(nums[i]==el2) cn2++;
         }
-        //System.out.println(mpp);
 
-        for(Map.Entry<Integer,Integer> it: mpp.entrySet())
-        {
-            if(it.getValue()>nums.length/3)
-            ans.add(it.getKey());
-        }
+        int maj= (int)(nums.length/3) +1;
+
+        if(cn1>=maj) ans.add(el1);
+        if(cn2>=maj) ans.add(el2);
+
         return ans;
     }
 }
