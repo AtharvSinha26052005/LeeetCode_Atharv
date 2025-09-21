663471fc58e5e7bcf6e758e83091e6065c254c5f
Atharv Sinha <73791750+AtharvSinha26052005@users.noreply.github.com>
2025-09-21 17:21:08 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (73.82%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -4,6 +4,7 @@ class Solution {
         //well will take a prefix product (similar to prefix sum)
         // and a suffix product (similar to suffix sum)
         // and when 0 occurs set product = 1
+        //This is using kadane's algo from fron and back and storing th max out of both
 
         int maxi=Integer.MIN_VALUE; int pre=1,suff=1;
 
