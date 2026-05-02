6cc4533287e5e80dd847eea6ddca27b64c6889f8
Atharv Sinha <73791750+AtharvSinha26052005@users.noreply.github.com>
2026-05-02 14:35:21 +0530

    Time: 4 ms (59.28%) | Memory: 41.8 MB (96.61%) - LeetSync

--- 804-rotated-digits/rotated-digits.java
+++ 804-rotated-digits/rotated-digits.java
@@ -1,36 +1,46 @@
 class Solution {
     public int rotatedDigits(int n) 
     {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(0, 0);
-        map.put(1, 1);
-        map.put(2, 5);
-        map.put(5, 2);
-        map.put(6, 9);
-        map.put(8, 8);
-        map.put(9, 6);
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // map.put(0, 0);
+        // map.put(1, 1);
+        // map.put(2, 5);
+        // map.put(5, 2);
+        // map.put(6, 9);
+        // map.put(8, 8);
+        // map.put(9, 6);
 
 
         int c=0;
-        //StringBuilder sb = new StringBuilder();
         for(int i=1;i<=n;i++)
         {
-            int num=i,d=0,f=0;
-            String num1="";
+            int num=i;
+            boolean valid=true;
+            boolean diff=false;
             while(num>0)
             {
-                d=num%10;
-                if(!map.containsKey(d)) {f=1;break;}
-                num1= String.valueOf(map.get(d)) + num1;
+                int d=num%10;
+                
+                if(d==3||d==4||d==7)
+                {
+                    valid=false;break;
+                }
+
+                else if (d==2 || d==5 || d==6 || d==9)
+                {
+                    diff=true;
+                }
+
                 num/=10;
             }
 
-            if(f==1) continue;
+           // if(f==1) continue;
             
-
-            if(i!=Integer.parseInt(num1))
-            {System.out.print(num1 + " ");c++;
+            if(valid && diff)
+            {
+                c++;
             }
+
         }
 
         return c;
