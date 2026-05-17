b8e6f3141f6fc0b4c14994404fff05d13f55a2a6
Atharv Sinha <73791750+AtharvSinha26052005@users.noreply.github.com>
2026-05-17 22:05:39 +0530

    Time: 10 ms (22.65%) | Memory: 48.5 MB (80.05%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.java
+++ 1428-jump-game-iii/jump-game-iii.java
@@ -1,26 +1,40 @@
 class Solution {
     public boolean canReach(int[] arr, int start) 
     {
-        return solve(arr, start);
+        int n=arr.length;
+
+        boolean vis[]= new boolean[n];
+        Arrays.fill(vis,false);
+        Queue<Integer> q = new LinkedList();
+
+        q.offer(start);
+        vis[start] = true;
+
+        while(!q.isEmpty())
+        {
+            int i=q.poll();
+          
+             if(arr[i]==0) return true;
+
+             int a=i+arr[i];
+             int b = i-arr[i];
+
+             if(a<n && a>=0 && !vis[a])
+             {
+                vis[a] = true;
+                q.offer(a);
+             }
+
+             if(b<n && b>=0 && !vis[b])
+             {
+                vis[b] = true;
+                q.offer(b);
+             }
+        }
+
+        return false;
+
+
     }
-
-    static boolean solve(int arr[], int i)
-    {
-        int n = arr.length;
-
-        if(i < 0 || i >= n || arr[i] == -1)
-            return false;
-
-        if(arr[i] == 0)
-            return true;
-
-        int jump = arr[i];
-
-        arr[i] = -1; // mark visited
-
-        boolean a = solve(arr, i + jump);
-        boolean b = solve(arr, i - jump);
-
-        return a || b;
-    }
+    
 }
